1714abc4510a40881f8dcf29a5f467613bca2995
ethar1995 <71302338+ethar1995@users.noreply.github.com>
2020-12-19 04:14:14 +0300

    Update bikeshare.py
    description of def main

--- bikeshare.py
+++ bikeshare.py
@@ -197,7 +197,7 @@ def display_data(df):
     
 
 def main():
-	"""Main body of program"""
+	 """Main body of program"""
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
